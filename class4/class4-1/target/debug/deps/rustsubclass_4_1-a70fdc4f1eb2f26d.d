cce4c097422f3e926c44f20cdb69e39473a7fd1b
不想取名字 <74920047+pigpigfang@users.noreply.github.com>
2023-03-23 14:59:38 +0800

    Delete class4/class4-1/target directory

--- class4/class4-1/target/debug/deps/rustsubclass_4_1-a70fdc4f1eb2f26d.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/Lenovo/CLionProjects/RUST-sub/class4/class4-1/target/debug/deps/rustsubclass_4_1-a70fdc4f1eb2f26d: src/main.rs
-
-/mnt/c/Users/Lenovo/CLionProjects/RUST-sub/class4/class4-1/target/debug/deps/rustsubclass_4_1-a70fdc4f1eb2f26d.d: src/main.rs
-
-src/main.rs:
